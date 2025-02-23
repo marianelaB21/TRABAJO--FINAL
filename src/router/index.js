4fab459d3d46b2c60c96ceef4aeb696c2e19c1c5
marianelaB21 <maribustamante8907@gmail.com>
2025-02-23 14:58:00 -0300

    Pantalla de login

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+    path: '/Home',
+    name: 'HomeView',
+    component: HomeView
+  },
+  
 ]
 
 const router = createRouter({
